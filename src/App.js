400f6f856633c6bd040937dfcf718b4e0af60631
Germán Lugo <german.lugo@bitnibs.com>
2019-03-12 17:39:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Aid-to-Venezuela</p>
         </header>
-      </div>
+        <Button  variant="contained" color="primary"> Make a Donation </Button>
+        <Card> Card</Card>
+      </React.Fragment>
     );
   }
 }
